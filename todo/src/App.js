55660cb3e0de3ee37a3d9f238afc8192c7e422ce
Tanay Tapanshu <tanaytapanshu.com>
2020-07-30 01:48:11 +0530

    1st commit

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1>Hello Programmers 🚀</h1>
+  <input/>
+  <button>Hey man</button>
+    </div>
+  );
+}
+
+export default App;
