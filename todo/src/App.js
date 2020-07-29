21c4270b238200a72fbc62117227f52867193bc2
Tanay Tapanshu <tanaytapanshu.com>
2020-07-30 02:42:20 +0530

    Second commit

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [todos, setTodos]  = useState(['Wake up early', 'Go for a walk']);
+  const[input,setinput]= useState('');
+  console.log(input)
+
+  let addTodo = (event) => {
+    event.preventDefault();
+         console.log("hello")    
+         setTodos([...todos,input])
+
+  }
+
   return (
     <div className="App">
   <h1>Hello Programmers 🚀</h1>
-  <input/>
-  <button>Hey man</button>
+  <form>
+  <input value={input}  onChange={e => setinput(e.target.value) }/>
+  <button onClick={addTodo}>add the work</button>
+  </form>
+    <ul>
+      {todos.map(function(e)
+      {
+      return(<li>{e} <button>Delete</button> </li>)
+      })}
+    </ul>
     </div>
   );
 }
